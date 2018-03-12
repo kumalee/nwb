351b8516bc2be04378d4d2a5ec1301ee23b2d1e4
Jonny Buchanan <jonathan.buchanan@gmail.com>
2018-03-13 09:34:58 +1000

    Use a named class for the Express middleware test App component
    Fixes an error with react-hot-loader-loader@0.0.4, which there's a PR for

--- tests/fixtures/projects/express-middleware/src/App.js
+++ tests/fixtures/projects/express-middleware/src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 
-export default class extends Component {
+export default class App extends Component {
   render() {
     return <div>
       <h2>Welcome to React</h2>
